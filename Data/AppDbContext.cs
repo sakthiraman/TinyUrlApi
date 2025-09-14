2892fbf1e3912ca46c2aaca30d1a204a7629baf9
test <test>
2025-09-14 16:12:29 +0400

    Update AppDbContext.cs

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,6 +6,6 @@ namespace TinyUrlApi
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    public DbSet<UrlMapping> UrlMappings { get; set; } 
+    public DbSet<UrlMapping> UrlMappings { get; set; }  
 }
 }
